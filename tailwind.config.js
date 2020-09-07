33c89308c3eb2f2c2126e31d5c827aa58c50ab80
LeGaby <leo.gaboriau@gmail.com>
2020-09-07 13:26:00 +0200

    Added burger menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,11 +8,20 @@ module.exports = {
       borderRadius: {
         xl: '0.75rem',
       },
+      fill: {
+        primary: '#338aff',
+      },
       flex: {
         full: '0 0 100%',
       },
       height: {
         'almost-full': 'calc(100vh - 5rem)',
+      },
+      inset: {
+       '1/2': '50%',
+      },
+      scale: {
+        250: '2.5',
       },
       textColor: {
         primary: '#338aff',
